df078592b2a7a12c012b2fd535d8fbef3c12f3a1
scchang <a0928136004@gmail.com>
2023-08-21 13:02:25 +0800

    modified

--- main.py
+++ main.py
@@ -16,13 +16,10 @@ import requests
 PRICE_PCM = 17
 
 def create_chrome_driver()->webdriver:
-    options = Options()
     options.add_argument("--headless")
-    
     # for windows
     current_cwd = os.path.abspath(os.getcwd())
     options.chrome_executable_path=f"{current_cwd}\chromedriver-win64\chromedriver.exe"
-
     # for linux
     # unzip chromedriver_linux64.zip
     # sudo mv chromedriver /usr/local/bin/ 安裝 chromedriver
@@ -35,13 +32,14 @@ def create_chrome_driver()->webdriver:
 def close_chrome_driver(dr:webdriver)->None:
     dr.close()
 
+    options = Options()
 def login_meters_page(dr:webdriver)->None:
     print("連線至水錶登入頁面")
     dr.get("http://www.cnyiot.com/MLogin.aspx")
     time.sleep(3)
-    print("自動輸入帳密並登入")
     username_input = dr.find_element(By.ID, "username")
     password_input = dr.find_element(By.ID, "password")
+    print("自動輸入帳密並登入")
     username_input.send_keys("00011049")
     password_input.send_keys("14725818")
     btn_singin = dr.find_element(By.ID, "subBt")
@@ -90,7 +88,7 @@ def get_meter_history(dr:webdriver, meter:str)->(list, float):
     print(f"連線至水錶 {meter} 歷史記錄頁面")
     url = f"http://www.cnyiot.com/MMpublicHis.aspx?ID={meter}"
     dr.get(url)
-    time.sleep(3)
+    time.sleep(5)
     print(f"取得水錶 {meter} 歷史記錄")
     meter_element = dr.find_element(By.ID, "table1") 
     # print(meter_element.text)
@@ -122,22 +120,20 @@ def save_meter_history_csv(meter:str, data:list, usage:float, price_pcm: int)->N
         writer.writeheader()
         writer.writerows(data)
     
-    bill = round(price_pcm * usage)
+    bill = price_pcm * usage
+    bill_str = "%.1f"%bill
     usage_str = "%.1f"%usage
     with open(filename, mode='a', encoding='utf-8', newline='') as file:
         file.writelines(f"""使用總水量(度),{usage_str}
 每度水(元),{price_pcm}
-金額(元),{bill}""")
+金額(元),{bill_str}""")
 
 def send_line_notify(msg:str)->requests.Response:
     url = 'https://notify-api.line.me/api/notify'
     # 個人測試
     token = 'pbgUOOVbAa9HgFWJRixB6zIdrOEvK7KQ2FWeoZGZTiJ'
-    # 新富昌群組用
-    # token = 'al3yDsfErCVsK5XQjahLKTGiG2dpyjbAg1F2CTuyKWQ'
     headers = {
         'Authorization': 'Bearer ' + token, # 設定權杖
-        'Content-Type' : 'application/json'
     }
     params = {
         'message': msg # 設定要發送的訊息
@@ -164,7 +160,8 @@ def send_meter_history_line_notify(meter:str, data:list, usage:float, price_pcm:
     usage = 0.0
     for i in data:
         usage += float(i['使用水量'])
-    bill = round(price_pcm * usage)
+    bill = price_pcm * usage
+    bill_str = "%.1f"%bill
     usage_str ="%.1f"%usage
     current_date = str(datetime.date.today())
     current_month = current_date[:7]
@@ -181,8 +178,7 @@ def send_meter_history_line_notify(meter:str, data:list, usage:float, price_pcm:
     [結束總水量(度):{data[0]['結束總水量']}]
     [總使用水量(度):{usage_str}]
     [每度水(元):{price_pcm}]
-    [金額(元):{bill}]
-    """
+    [金額(元):{bill_str}]"""
     print(f"傳送水錶 {meter} 歷史記錄至LINE")
     return send_line_notify(msg)
 
@@ -192,8 +188,7 @@ def send_bill_total_line_notify(name:str, bill:int)->requests.Response:
     msg:str=f"""
     [{name}]
     [用水月份:{current_month}]
-    [總金額(元):{bill}]
-    """
+    [總金額(元):{bill}]"""
     return send_line_notify(msg)
 
 def main():
@@ -203,20 +198,21 @@ def main():
     send_meters_management_line_notify(meters_management)
     save_meters_management_csv(meters_management)
     print("\n")
-    bill_G12, bill_21Century = 0, 0
+    bill_G12:int = 0
+    bill_21Century:int = 0
     for i in meters_management:
         meter_history, usage = get_meter_history(driver, i["水錶號碼"])
         send_meter_history_line_notify(i["水錶名稱"],meter_history, usage, PRICE_PCM)
         save_meter_history_csv(i["水錶名稱"], meter_history, usage, PRICE_PCM)
         print("\n")
         if(i["水錶名稱"]=='1F前面水錶' or i["水錶名稱"]=='2F前面水錶'):
-            bill_21Century += round(usage*PRICE_PCM)
+            bill_21Century += (usage*PRICE_PCM)
         else:
-            bill_G12 += round(usage*PRICE_PCM)
+            bill_G12 += (usage*PRICE_PCM)
     close_chrome_driver(driver)
-    send_bill_total_line_notify("G12汽車", bill_G12)
+    send_bill_total_line_notify("G12汽車", round(bill_G12))
     time.sleep(3)
-    send_bill_total_line_notify("21世紀", bill_21Century)
+    send_bill_total_line_notify("21世紀", round(bill_21Century))
 
 if __name__ == "__main__":
     main()
